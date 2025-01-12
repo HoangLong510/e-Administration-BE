9ee055e068f1804bdb88042003f1ddb6805758fb
HoangLong510 <hoanglongnguyen0510@gmail.com>
2025-01-12 17:49:03 +0700

    [Long] add Dto for edit task

---
+++ Server/DTOs/Tasks/EditTaskDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Server.DTOs.Tasks
+{
+    public class EditTaskDto
+    {
+        public int Id { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int AssigneesId { get; set; }
+    }
+}
